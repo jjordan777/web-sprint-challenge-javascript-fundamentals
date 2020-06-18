a53966ec5d9a34e14a7932ef83fe054a449ee720
James Jordan <jjordan777@gmail.com>
2020-06-18 13:38:56 -0700

    task 2 complete

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -125,6 +125,10 @@ console.log(populationTotal);
  * The last parameter accepts a callback
  * The consume function should return the invocation of cb, passing a and b into cb as arguments
  */
+function consume(a,b,cb){
+  
+
+}
 
 /* Step 2: Create several functions to callback with consume();
  * Create a function named add that returns the sum of two numbers
